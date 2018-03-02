99be94c9ba5b26677dd59e3cf65e41b60f32b9bb
Derrick <derrick.valentine@gmail.com>
2018-03-01 22:08:04 -0500

    Working basic Todo list with bootstrap and stored in mongodb

---
+++ app.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+//port
+const port = 3000;
+
+//Init app
+const app = express();
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+const url = 'mongodb://localhost:27017/todoapp';
+
+//Body Parser Middleware
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, '/public')));
+
+//View Setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+// app.use(express.static(path.join(_dirname, '/public')));
+//Connect to mongodb
+MongoClient.connect(url, (err, database) => {
+  console.log('mo ngodb connected...');
+  if (err) throw err;
+
+  db = database.db('todoapp');
+  Todos = db.collection('todos');
+
+  app.listen(port, () => {
+    console.log('Server running on port ' + port);
+  });
+
+});
+
+app.get('/', (req,res,next) => {
+  Todos.find({}).toArray((err, todos) => {
+    if (err) {
+      return console.log(err);
+    }
+    res.render('index', {
+      todos: todos
+    });
+  });
+});
+
+app.get('/todo/edit/:id', (req,res,next) => {
+  const query = {_id: ObjectID(req.params.id)}
+  Todos.find(query).next((err, todo) => {
+    if(err) {
+      return console.log(err);
+    }
+    res.render('edit', {
+      todo: todo
+    });
+  });
+});
+
+app.post('/todo/add', (req, res, next) => {
+  Todos.insert({title: req.body.title, body: req.body.description});
+  res.redirect('/');
+});
+
+app.post('/todo/edit/:id', (req, res, next) => {
+  const query = {_id: ObjectID(req.params.id)}
+  const todo = {
+    title: req.body.title,
+    body: req.body.description
+  }
+  Todos.updateOne(query, {$set:todo}, (err, result) => {
+    if (err) {
+      return console.log(err);
+    }
+  })
+  res.redirect('/');
+});
+
+app.delete('/todo/delete/:id', (req, res, next) => {
+  const query = {_id: ObjectID(req.params.id)}
+  Todos.deleteOne(query, (err, response) => {
+    if(err) {
+      return console.log(err);
+    }
+    console.log('Todo Removed');
+    res.send(200);
+  });
+});
